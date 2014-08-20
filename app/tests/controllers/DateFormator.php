30d554a1a2a49feb83229009e77ae36f472c70f5
sudebkumar <sudeb.phy137@gmail.com>
2014-08-20 12:27:21 +0600

    initial commit

---
+++ app/tests/controllers/DateFormator.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LAB5
+ * Date: 8/20/14
+ * Time: 11:04 AM
+ */
+
+class DateFormatter{
+
+protected $stamp;
+    public function _construct(DateTime $stamp)
+    {
+        $this->stamp = $stamp;
+    }
+
+    public function getStamp()
+    {
+        return $this->stamp;
+    }
+}
+
+public function testStampMustBeInstanceOfDateTime()
+{
+
+    $date = new DateFormatter(new DateTime);
+
+    $this->assertInstanceOf('DateTime', $date->getStamp());
+}
