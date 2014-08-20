ff5bf4012a5c0a11038904dc511edb0e6d141558
sudebkumar <sudeb.phy137@gmail.com>
2014-08-20 14:18:33 +0600

    commit

--- app/tests/PracticeTest.php
+++ app/tests/PracticeTest.php
@@ -5,6 +5,63 @@ class PracticeTest extends PHPUnit_Framework_TestCase{
     public function testHelloWorld()
     {
         $greeting = 'Hello,World.';
-        $this->assertTrue($greeting == 'Hello,World.',$greeting);
+        $this->assertTrue($greeting == 'Hello,World.');
+    }
+
+
+    public function testHi()
+    {
+        $greeting = 'Hello,World.';
+        $this->assertFalse($greeting == 'Hello,World',$greeting);
+    }
+
+
+    public function testYourName()
+    {
+        $greeting = 'YourName';
+        $this->assertEquals('YourName',$greeting);
+    }
+
+    public function testLaravelDevsIncludesDayle()
+    {
+        $name = ['Taylor','Shawn','Dayle'];
+        $this->assertContains('Dayle',$name);
+    }
+
+    public function testValue()
+    {
+        $val = 0;
+        $this->assertEquals(0,$val);
+    }
+
+
+    public function testNumber()
+    {
+        $val = null;
+        $this->assertEquals(0,$val);
+    }
+
+
+    public function testFamilyRequiresParent()
+    {
+        $family = [
+            'parents' => 'joe',
+            'children' => ['Timmy','Suzy']
+        ];
+
+        $this->assertInternalType('array', $family['children']);
+    }
+
+    public function testInteger()
+    {
+        $age = 25;
+
+
+        $this->assertInternalType('integer', $age);
     }
 }
+
+
+
+
+
