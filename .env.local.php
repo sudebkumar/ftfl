1e1afa7badc54e7311d92136d487befc3461912c
sudebkumar <sudeb.phy137@gmail.com>
2014-08-21 16:42:22 +0600

    .env.local.php todolist commit

---
+++ .env.local.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    // Database vars
+    'DB_HOST' => 'localhost.localdomain',
+    'DB_USER' => 'root',
+    'DB_PASS' => 'laravel',
+    'DB_NAME' => 'todolist',
+
+    // Any other vars you want
+    // ...
+];
