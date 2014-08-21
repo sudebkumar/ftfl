9740e608e62db3bf40612e392b37aa08a98e6be8
sudebkumar <sudeb.phy137@gmail.com>
2014-08-21 13:10:37 +0600

    bootstrap start.php todo list commit

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,12 +24,9 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('homestead'),
-
-));
-
+$env = $app->detectEnvironment(function() {
+    return 'local';
+});
 /*
 |--------------------------------------------------------------------------
 | Bind Paths
