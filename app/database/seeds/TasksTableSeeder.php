72423e0a12ceaa86e5aa49315bab9f777e577162
sudebkumar <sudeb.phy137@gmail.com>
2014-08-24 08:57:55 +0600

    TaskTableSeeder.php todolist commit

---
+++ app/database/seeds/TasksTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+class TasksTableSeeder extends Seeder {
+
+    public function run()
+    {
+        // Uncomment the below to wipe the table clean before populating
+        // DB::table('tasks')->truncate();
+
+        $tasks = array(
+
+            ['name' => 'Task 1', 'slug' => 'task-1', 'project_id' => 1, 'description' => 'My first task', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['name' => 'Task 2', 'slug' => 'task-2', 'project_id' => 1, 'description' => 'My second task', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['name' => 'Task 3', 'slug' => 'task-3', 'project_id' => 1, 'description' => 'My third task', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['name' => 'Task 4', 'slug' => 'task-4', 'project_id' => 2, 'description' => 'My fourth task', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['name' => 'Task 5', 'slug' => 'task-5', 'project_id' => 2, 'description' => 'My fifth task', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            );
+
+        // Uncomment the below to run the seeder
+
+        DB::table('tasks')->insert($tasks);	}
+
+}
