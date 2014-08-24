4c40f560d1fc9a3d7903ae2503fa01fbd9f333d6
sudebkumar <sudeb.phy137@gmail.com>
2014-08-24 12:21:47 +0600

    show.blade.php todolist commit

---
+++ app/views/projects/show.blade.php
@@ -0,0 +1,13 @@
+
+@section('main')
+    <h2>{{ $project->name }}</h2>
+@if ( !$project->tasks->count() )
+    Your project has no tasks.
+@else
+		<ul>
+@foreach( $project->tasks as $task )
+				<li><a href="{{ route('projects.tasks.show', [$project->slug, $task->slug]) }}">{{ $task->name }}</a></li>
+@endforeach
+		</ul>
+@endif
+@stop
