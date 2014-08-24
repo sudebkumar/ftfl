71ee855135e80342f7be5fe3b867c972aeddc458
sudebkumar <sudeb.phy137@gmail.com>
2014-08-24 08:56:53 +0600

    ProjectTableSeeder.php todolist commit

---
+++ app/database/seeds/ProjectsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+class ProjectsTableSeeder extends Seeder {
+
+    public function run()
+    {
+        // Uncomment the below to wipe the table clean before populating
+        // DB::table('projects')->truncate();
+
+        $projects = array(
+
+            ['name' => 'Project 1', 'slug' => 'project-1', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['name' => 'Project 2', 'slug' => 'project-2', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['name' => 'Project 3', 'slug' => 'project-3', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            );
+
+        // Uncomment the below to run the seeder
+
+        DB::table('projects')->insert($projects);	}
+
+}
